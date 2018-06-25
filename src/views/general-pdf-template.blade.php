3eb305a70a83bab714030157ceee1411dd25e6fa
Samuel Terra <samuelterra22@hotmail.com>
2018-06-25 12:42:52 -0300

    Rollback translate texts

--- src/views/general-pdf-template.blade.php
+++ src/views/general-pdf-template.blade.php
@@ -290,13 +290,13 @@ if ($showTotalColumns != []) {
 <script type="text/php">
 	    	@if (strtolower($orientation) == 'portrait')
         if ( isset($pdf) ) {
-            $pdf->page_text(30, ($pdf->get_height() - 26.89), "Data de impressão: " . date('d M Y H:i:s'), null, 10);
-            $pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Página {PAGE_NUM} de {PAGE_COUNT}", null, 10);
+            $pdf->page_text(30, ($pdf->get_height() - 26.89), "Date Printed: " . date('d M Y H:i:s'), null, 10);
+            $pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10);
         }
 @elseif (strtolower($orientation) == 'landscape')
         if ( isset($pdf) ) {
-            $pdf->page_text(30, ($pdf->get_height() - 26.89), "Data de impressão: " . date('d M Y H:i:s'), null, 10);
-            $pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Página {PAGE_NUM} de {PAGE_COUNT}", null, 10);
+            $pdf->page_text(30, ($pdf->get_height() - 26.89), "Date Printed: " . date('d M Y H:i:s'), null, 10);
+            $pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10);
         }
 @endif
 </script>
